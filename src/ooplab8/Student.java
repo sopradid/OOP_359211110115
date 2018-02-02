09ec0b8fab75b2643c381b2760780035d9282a65
LAB_COM-MT_1 <may14082538@gmail.com>
2018-02-02 09:16:01 +0700

    person and Student

---
+++ src/ooplab8/Student.java
@@ -0,0 +1,29 @@
+package ooplab8;
+
+public class Student extends Person {
+    private String studentID;
+    private String major;
+
+    public Student(String personID, String name,
+                   String address, String gender,String studentID,String major) {
+        super(personID, name, address, gender);
+        this.studentID = studentID;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentID='" + studentID + '\'' +
+                ", major='" + major + '\'' +
+                "} " + super.toString();
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+}//class
